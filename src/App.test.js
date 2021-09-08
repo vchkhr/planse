c9634b08d1b7961e7b3f590d141f02b6bf3621ff
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-09-08 16:29:52 +0300

    Fix npm test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('You are not logged in', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/You are not logged in/i);
   expect(linkElement).toBeInTheDocument();
-});
+})
